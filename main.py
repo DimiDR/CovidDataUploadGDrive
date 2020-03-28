3367382169a3716ec8896955248d9fb66cf035c5
Dimitri Rybak <dima_rybak@yahoo.de>
2020-03-28 13:28:52 +0100

    everything was reworked

--- main.py
+++ main.py
@@ -1,93 +1,40 @@
-
-#Video: https://www.youtube.com/watch?v=9OYYgJUAw-w
-from __future__ import print_function
-import httplib2
-import os, io
+#https://towardsdatascience.com/how-to-manage-files-in-google-drive-with-python-d26471d91ecd
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 import requests
-import pickle
-
 from datetime import datetime, timedelta
-from apiclient import discovery, errors
-from oauth2client import client
-from oauth2client import tools
-from oauth2client.file import Storage
-#from apiclient.http import MediaFileUpload, MediaIoBaseDownload
-from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
-import auth
-# If modifying these scopes, delete your previously saved credentials
-# at ~/.credentials/drive-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/drive'
-CLIENT_SECRET_FILE = 'client_secrets.json'
-APPLICATION_NAME = 'Drive API Python Quickstart'
-authInst = auth.auth(SCOPES,CLIENT_SECRET_FILE,APPLICATION_NAME)
-credentials = authInst.getCredentials()
-DRIVE_FOLDER_ID = '1RovjMstZWYNIQevwvjaaiKt2GuKw3Ks4'
 
-http = credentials.authorize(httplib2.Http())
-drive_service = discovery.build('drive', 'v3', http=http)
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth() # client_secrets.json need to be in the same directory as the script
+drive = GoogleDrive(gauth)
 
-def listFiles(size):
-    results = drive_service.files().list(
-        pageSize=size,fields="nextPageToken, files(id, name)").execute()
-    items = results.get('files', [])
-    if not items:
-        print('No files found.')
-    else:
-        print('Files:')
-        for item in items:
-            print('{0} ({1})'.format(item['name'], item['id']))
+# Put in your folder ID here
+FOLDER_ID = '1RovjMstZWYNIQevwvjaaiKt2GuKw3Ks4'
 
-def uploadFile(filename,filepath,mimetype):
-    file_metadata = {'name': filename, "parents": '1RovjMstZWYNIQevwvjaaiKt2GuKw3Ks4'}
-    media = MediaFileUpload(filepath,
-                            mimetype=mimetype, 
-                            resumable=False)
-    file = drive_service.files().create(body=file_metadata,
-                                        media_body=media,
-                                        fields='id').execute()
-    print('File ID: %s' % file.get('id'))
-    save_filename(file.get('id'))
+def uploadNewFile(filename,FOLDER_ID,mimetype):
+    file1 = drive.CreateFile({"mimeType": mimetype, "parents": [{"kind": "drive#fileLink", "id": FOLDER_ID}]})
+    file1.SetContentFile(filename)
+    try:
+        file1.Upload() # Upload the file.
+        print("Upload successful. ID: {}".format(file1.get('id')))
+    except:
+        print("Upload failed. ID: {}".format(file1.get('id')))
 
-def downloadFile(file_id,filepath):
-    request = drive_service.files().get_media(fileId=file_id)
-    fh = io.BytesIO()
-    downloader = MediaIoBaseDownload(fh, request)
-    done = False
-    while done is False:
-        status, done = downloader.next_chunk()
-        print("Download %d%%." % int(status.progress() * 100))
-    with io.open(filepath,'wb') as f:
-        fh.seek(0)
-        f.write(fh.read())
-
-def createFolder(name):
-    file_metadata = {
-    'name': name,
-    'mimeType': 'application/vnd.google-apps.folder'
-    }
-    file = drive_service.files().create(body=file_metadata,
-                                        fields='id').execute()
-    print ('Folder ID: %s' % file.get('id'))
-
-def searchFile(size,query):
-    results = drive_service.files().list(
-    pageSize=size,fields="nextPageToken, files(id, name, kind, mimeType)",q=query).execute()
-    items = results.get('files', [])
-    if not items:
-        print('No files found.')
-    else:
-        print('Files:')
-        for item in items:
-            print(item)
-            print('{0} ({1})'.format(item['name'], item['id']))
+def deleteOldFile(file_name):
+    # search fr file id
+    fileList = drive.ListFile({'q': "'1RovjMstZWYNIQevwvjaaiKt2GuKw3Ks4' in parents and trashed=false"}).GetList()
+    for file in fileList:
+        # Get the folder ID that you want
+        if(file['title'] == file_name):
+            file_id = file['id']
+    try:
+        file2 = drive.CreateFile({'id': file_id}) # Initialize GoogleDriveFile instance with file id.
+        file2.Delete() # Permanently delete the file.
+        print("File was deleted from Drive. ID: {}".format(file['id']))
+    except:
+        print('No file to delete')
 
 def getCovidData(d):
-    
     year = d.year
     month = d.month
     day = d.day
@@ -108,32 +55,12 @@ def getCovidData(d):
     else:
         open('covid.xlsx', 'wb').write(r.content)
         print("Download successful")
-    
-def delete_file(service, file_id):
-    print("Trying to delete - {}".format(file_id))
-    try:
-        service.files().delete(fileId=file_id).execute()
-        print("File was deleted from Drive")
-    except:
-        print('No file to delete')
 
-def save_filename(id):
-    pickle.dump([id], open("id.p", "wb"))
-
-def load_filename():
-    return pickle.load(open("id.p","rb"))
-
-# Execution
-last_file_id = ''
-try:
-    last_file_id = load_filename()
-except:
-    print("no file")
-delete_file(drive_service, last_file_id)
-d = datetime.today()
+#Execution
+print("---Downloading New Covid Data---")
+d = datetime.today() # outside def because of recursion
 getCovidData(d)
-uploadFile('covid.xlsx','covid.xlsx','application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-#uploadFile('unnamed.jpg','unnamed.jpg','image/jpeg')
-#downloadFile('1Knxs5kRAMnoH5fivGeNsdrj_SIgLiqzV','google.jpg')
-#createFolder('Google')
-#searchFile(10,"name contains 'Getting'")
+
+print("---Updating Covid Data on Drive---")
+deleteOldFile('covid.xlsx')
+uploadNewFile('covid.xlsx', FOLDER_ID ,'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
